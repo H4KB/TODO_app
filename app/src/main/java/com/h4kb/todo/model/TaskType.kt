47bc97dfb81a435026ccbcdba872354355b1c1af
H4KB <akabaharuka@gmail.com>
2021-07-03 21:35:52 +0900

    [feat] Add task Model.

---
+++ app/src/main/java/com/h4kb/todo/model/TaskType.kt
@@ -0,0 +1,6 @@
+package com.h4kb.todo.model
+
+enum class TaskType {
+    TODO,
+    DONE
+}
---
+++ app/src/main/java/com/h4kb/todo/model/ToDoRepository.kt
@@ -0,0 +1,19 @@
+package com.h4kb.todo.model
+
+object ToDoRepository {
+    suspend fun createTask(todo : TaskModel) {
+
+    }
+
+    suspend fun deleteTask(todo: TaskModel) {
+
+    }
+
+    suspend fun moveToDone(todo: TaskModel) {
+
+    }
+
+    suspend fun moveToToDo(todo: TaskModel) {
+
+    }
+}
