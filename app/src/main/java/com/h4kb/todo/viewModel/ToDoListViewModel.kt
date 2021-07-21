dfe41ee88fc0abf2b9a7cd2c1e6af570ebd65039
H4KB <akabaharuka@gmail.com>
2021-07-21 23:22:02 +0900

    Prepare to open the dialog to create task

--- app/src/main/java/com/h4kb/todo/viewModel/ToDoListViewModel.kt
+++ app/src/main/java/com/h4kb/todo/viewModel/ToDoListViewModel.kt
@@ -1,9 +1,7 @@
 package com.h4kb.todo.viewModel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.distinctUntilChanged
 import com.h4kb.todo.model.TaskModel
 import com.h4kb.todo.model.TaskType
 import java.util.*
@@ -12,6 +10,16 @@ class ToDoListViewModel : ViewModel() {
     private val taskRow = mutableListOf<TaskModel>()
     private val _tasks by lazy { MutableLiveData<List<TaskModel>>() }
     val tasks = _tasks
+
+    val isCreateTaskDialogVisible: MutableLiveData<Boolean> = MutableLiveData<Boolean>(false)
+
+    fun openCreateTaskDialog() {
+        isCreateTaskDialogVisible.value = true;
+    }
+
+    fun closeCreateTaskDialog() {
+        isCreateTaskDialogVisible.value = false
+    }
 
     fun addTask() {
         val cal = Calendar.getInstance()
